b70d5f5824656e5fe8bbd9e077c13d4765463162
Subrata Roy <113381826+Subrata41@users.noreply.github.com>
2023-01-27 00:16:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
-class Request {
-    constructor(url, method = 'GET') {
-        this.url = url;
-        this.method = method;
-        this.#secure = url.startsWith('https') ? true : false;
-    }
-    
-    isSecure() {
-        return this.#secure;
-    }
-    
-    updateURL(new_url) {
-        this.url = new_url;
-        this.#secure = new_url.startsWith('https') ? true : false;
-    }
-}
 
-const req = new Request('https://example.com');
-console.log(req.isSecure()); 
-req.updateURL('http://example.com');
-console.log(req.isSecure()); 
  
 
 
