c5f604064ee7e4e52bbe9dfd46992cdc360a715c
Subrata Roy <113381826+Subrata41@users.noreply.github.com>
2023-01-27 00:09:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+class API {
+    constructor(url, method = 'GET') {
+        this.url = url;
+        this.method = method;
+        #secure = url.startsWith("https") ? true : false;
+    }
+
+    isSecure() {
+        return #secure;
+    }
+
+    updateUrl(newUrl) {
+        this.url = newUrl;
+        #secure = newUrl.startsWith("https") ? true : false;
+    }
+}
+
+const s = new API('http://api.com/api/hello');
+console.log(s.isSecure()); 
+
+s.updateUrl('https://api.com/api/hello');
+console.log(s.isSecure()); 
+
+console.log(s.url); 
+console.log(s.method); 
+console.log(s.secure); 
 
 
 module.exports = { API }
