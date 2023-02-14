82e9714d4d75d15141017eaeac977abf948590ac
Subrata Roy <113381826+Subrata41@users.noreply.github.com>
2023-02-14 22:51:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 
- 
-
+class API{
+    #secure;
+    url;
+    method;
+    constructor(url){
+        this.url=url;
+        if(url.includes("https"))
+        this.#secure=true;
+        else
+        this.#secure=false;
+        this.method="GET"
+    }
+    isSecure(){
+        return this.#secure;
+    }
+    updateUrl(url){
+        this.url=url
+        if(url.includes("https"))
+        this.#secure=true;
+        else
+        this.#secure=false;
+    }
+}
 
 module.exports = { API }
